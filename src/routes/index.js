8b89b5b86fe8fc615c84d9dbcf1f30fa70297877
knocknockguesswho <nullengineer@gmail.com>
2020-08-13 10:59:00 +0700

    updateng local repo

--- src/routes/index.js
+++ src/routes/index.js
@@ -69,7 +69,7 @@ const TabProfile = () => {
 
 const App = () => {
   return (
-    <Stack.Navigator initialRouteName="Search">
+    <Stack.Navigator initialRouteName="MainApp">
       <Stack.Screen
         name="AddShippingAddress"
         component={AddShippingAddress}
