0f9f927541b52f50d57aa418309fdd97609ccd8f
Dimas Mokodompit <dimasdompit@gmail.com>
2020-08-13 09:30:47 +0700

    add connect redux for shop

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -24,7 +24,7 @@ const Home = (props) => {
     await props
       .getAllProducts(token)
       .then((response) => {
-        console.log(response);
+        // console.log(response);
         setNewest(response.value.data.data);
       })
       .catch((error) => console.log(error.response));
